d93d412d38afe47d4bc929133d886e650142174c
edwindn <edn.work@proton.me>
2025-01-06 22:49:43 +0000

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,6 +1,13 @@
 import torch
 import torch.nn as nn
-from audio import get_spectrogram
+from audio import load_audio, pad_or_trim, get_spectrogram
 from model import Whisper, WhisperParams
-
+from transformers import PreTrainedTokenizerFast
+tokenizer = PreTrainedTokenizerFast(tokenizer_file="tokenizer.json")
 whisper = Whisper(WhisperParams())
+
+audio = load_audio('english1.mp3')
+spec = get_spectrogram(torch.tensor(audio))
+spec = pad_or_trim(spec, length=1500, axis=1)
+out = whisper(spec.unsqueeze(0), tokens=torch.tensor([50258, 50364, 50257], dtype=torch.float32)) # using blank tokens
+print(out.shape)
