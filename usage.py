3d29961ad8b118aa002a962a7ed172a90a75653d
edwindn <edn.work@proton.me>
2025-01-06 23:54:47 +0000

    Update usage.py

--- usage.py
+++ usage.py
@@ -9,7 +9,6 @@ tokenizer = PreTrainedTokenizerFast(tokenizer_file="tokenizer.json")
 params = WhisperParams()
 whisper = Whisper(params)
 whisper.float()
-print('loaded model')
 
 test_filename = 'audio/english1.mp3'
 
@@ -18,9 +17,10 @@ spec = get_spectrogram(torch.tensor(audio))
 spec = pad_or_trim(spec, length=params.audio_seq_len, axis=1)
 
 tokens = torch.tensor([50258, 50364, 50257], dtype=torch.int64)
-tokens = pad_or_trim(tokens, length=params.txt_seq_len, axis=0)
+#tokens = pad_or_trim(tokens, length=params.txt_seq_len, axis=0)
 
 print('padded model inputs')
 tokens = F.one_hot(tokens, num_classes=params.vocab_dim) # should be batch * seq length * embedding dim
+print('one hot encoded inputs')
 out = whisper(spec.unsqueeze(0), tokens) # using blank tokens
 print(out.shape)
