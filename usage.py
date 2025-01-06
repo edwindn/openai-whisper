ac5a97be643d845a26cef8bd2016a6e4c6e62106
edwindn <edn.work@proton.me>
2025-01-06 23:59:30 +0000

    Update usage.py

--- usage.py
+++ usage.py
@@ -17,10 +17,9 @@ spec = get_spectrogram(torch.tensor(audio))
 spec = pad_or_trim(spec, length=params.audio_seq_len, axis=1)
 
 tokens = torch.tensor([50258, 50364, 50257], dtype=torch.int64)
-#tokens = pad_or_trim(tokens, length=params.txt_seq_len, axis=0)
 
 print('padded model inputs')
-tokens = F.one_hot(tokens, num_classes=params.vocab_dim) # should be batch * seq length * embedding dim
+tokens = F.one_hot(tokens, num_classes=params.vocab_dim).unsqueeze(0) # should be batch * seq length * embedding dim
 print('one hot encoded inputs')
 out = whisper(spec.unsqueeze(0), tokens) # using blank tokens
 print(out.shape)
